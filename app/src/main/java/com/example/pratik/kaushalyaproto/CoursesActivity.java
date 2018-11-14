d172b482bf26bd2c178af5401d5ba28f14509d3c
driver <driverproject2018@gmail.com>
2018-11-14 08:23:08 +0530

    CoursesActivity

--- app/src/main/java/com/example/pratik/kaushalyaproto/CoursesActivity.java
+++ app/src/main/java/com/example/pratik/kaushalyaproto/CoursesActivity.java
@@ -1,13 +1,33 @@
 package com.example.pratik.kaushalyaproto;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.pratik.kaushalyaproto.CoursesFragments.AllCoursesFragment;
+import com.example.pratik.kaushalyaproto.CoursesFragments.MyCoursesFragment;
+import com.example.pratik.kaushalyaproto.adapters.TabAdapter;
+
 public class CoursesActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MyCoursesFragment(), "My Courses");
+        adapter.addFragment(new AllCoursesFragment(), "All Courses");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
